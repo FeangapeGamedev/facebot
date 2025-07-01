7b0968244dff0aa12e2a47d05a918261ae013e80
Felipe Garcia <felipe_garciapereira@trimble.com>
2025-07-01 21:11:09 +1200

    Fetch User from Api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import Cardlist from "./CarList";
-import { robots } from "./robots";
 import SearchBox from "./SearchBox";
 import "./App.css";
 
@@ -10,9 +9,17 @@ class App extends Component {
     super();
     // Initialize state with robots data and an empty search field
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: "",
     };
+  }
+
+  // Fetch user data from API when the component mounts
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json()) // Parse the JSON from the response
+      .then(users => {this.setState({ robots: users });  // Update the state with the fetched users
+      })
   }
 
   // Handler for updating the search field state when user types
@@ -26,7 +33,10 @@ class App extends Component {
       return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
     });
 
-    return (
+    if(this.state.robots.length === 0){
+      return <h1>Loading</h1>
+    } else{
+          return (
       <div className="tc">
        <h1 className="f-headline yellow mt4 mb3">FaceBots</h1>
         {/* SearchBox receives the handler as a prop */}
@@ -34,7 +44,8 @@ class App extends Component {
         {/* Cardlist receives the filtered robots as a prop */}
         <Cardlist robots={filteredRobots} />
       </div>
-    );
+     );
+    }
   }
 }
 
